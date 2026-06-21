639a1d14565cc4a239d2b9775ad7fc65e6b70779
Keybeen <keybeen.videoeditor@gmail.com>
2026-06-21 12:40:01 +0800

    config: update base settings and main URL routing for habits app

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,6 +42,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'poweratomic.accounts',
     'poweratomic.identities',
+    'poweratomic.habits',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('poweratomic.accounts.urls')),
     path('api/identities/', include('poweratomic.identities.urls')),
+    path('api/habits/', include('poweratomic.habits.urls')),
 ]
